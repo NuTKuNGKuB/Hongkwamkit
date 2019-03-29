a16967180d2ab928cf592b8f42f21b0663539da3
nitipong santiphapchai <nitipong.za1997@gmail.com>
2019-03-29 16:20:37 +0700

    Name

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import "rxjs/add/operator/map";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  members:any=0;
+  constructor(public navCtrl: NavController, public navParam: NavParams, public http: Http){
+    this.http.get('http://localhost:8080/member')
+    .map(res=>res.json())
+    .subscribe(data=>{
+      this.members = data;
+    });
   }
 
 }
